3da6185d1787f145f4cb49fa11242c2fe3e14acd
abramovmega <118120268+abramovmega@users.noreply.github.com>
2023-02-19 22:16:36 +0500

    1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,44 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let D = (b ** 2) - (4 * a * c);
+    let arr = [];
+
+    if (D < 0)
+        return arr;
+
+    if (a == 0)
+        return arr;
+
+    if (D == 0)
+        arr.push(-b / (2*a));
+    else if (D > 0) {
+        arr.push((-b + Math.sqrt(D)) / (2 * a))
+        arr.push((-b - Math.sqrt(D)) / (2 * a))
+    }
+    return arr;
 }
 
+console.log(solveEquation(1, -4, -5));
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-               
-  
+    if(isNaN(percent)){
+        return('pusto percent');
+    }
+    if(isNaN(contribution)){
+        return('pusto contribution');
+    }
+    if(isNaN(amount)){
+        return('pusto amount');
+    }
+    if(isNaN(countMonths)){
+        return('pusto countMonths');
+    }
+    monthlyPercentage = ((percent/100)/12);
+    loanBody = (amount-contribution);
+    payment = (loanBody*(monthlyPercentage+(monthlyPercentage/(((1+monthlyPercentage)**countMonths)-1))));
+    amountToBeRefunded = (payment*countMonths);
+    return amountToBeRefunded.toFixed(2);
 }
+console.log(calculateTotalMortgage(10, 0, 20000, 24.));
