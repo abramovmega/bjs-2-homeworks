e9db8b249f3f4b153990f1768f75cf27cfab30a8
abramovmega <abramov0206@mail.ru>
2023-02-21 00:11:34 +0500

    edit4

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,6 @@ function solveEquation(a, b, c) {
     let D = (b ** 2) - (4 * a * c);
     let arr = [];
 
-    if (D < 0) {
-        return arr;
-    }
     if (a == 0) {
         return arr;
     }
@@ -16,7 +13,6 @@ function solveEquation(a, b, c) {
         arr.push((-b + Math.sqrt(D)) / (2 * a))
         arr.push((-b - Math.sqrt(D)) / (2 * a))
     }
-
     return arr;
 }
 
@@ -26,13 +22,13 @@ console.log(solveEquation(1, -4, -5));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     if (isNaN(percent || contribution || amount || countMonths)) {
-        return('false');
+        return false;
     }
     let monthlyPercentage = ((percent/100)/12);
     let loanBody = (amount-contribution);
     let payment = (loanBody*(monthlyPercentage+(monthlyPercentage/(((1+monthlyPercentage)**countMonths)-1))));
     let amountToBeRefunded = (payment*countMonths);
-    return (+amountToBeRefunded.toFixed(2));
+    return (amountToBeRefunded.toFixed(2));
 }
     
 console.log(calculateTotalMortgage(10, 0, 20000, 24.));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,17 @@
 function getArrayParams(...arr) {
-
+  let min = (Math.min(...arr));
+  let max = (Math.max(...arr));
+  let sum = function (...arr){
+    let sum = 0;
+    for (let i =0; i < arr.length; i++){
+      sum += arr[i]
+    }
+  }
+  let avg = (sum/arr.length);
   return { min: min, max: max, avg: avg };
 }
+console.log(avg)
+/*console.log(getArrayParams(-99, 99, 10))*/
 
 function summElementsWorker(...arr) {
 
